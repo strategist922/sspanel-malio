6744d3d8696cf7353b0317f5be395f7e8455ea61
olperson <41310813+olperson@users.noreply.github.com>
2022-04-05 12:54:47 +0800

    Update Node.php 跟进上游更新：添加SS-AEAD单端口支持

--- app/Utils/URL.php
+++ app/Utils/URL.php
@@ -1046,8 +1046,15 @@ class URL
             if ($mu_user == null) {
                 return;
             }
-            $mu_user->obfs_param = $user->getMuMd5();
-            $mu_user->protocol_param = $user->id . ':' . $user->passwd;
+	        // 如果混淆和协议均为SS原生且为单端口的，即判断为AEAD单端口类型，密码配置为用户自身密码
+	        if ($mu_user->obfs == "plain" && $mu_user->protocol == "origin"){
+		        $mu_user->passwd = $user->passwd;
+		        $mu_user->obfs_param     = "";
+		        $mu_user->protocol_param = "";
+	        }else{
+		        $mu_user->obfs_param = $user->getMuMd5();
+		        $mu_user->protocol_param = $user->id . ':' . $user->passwd;
+	        }
             $user = $mu_user;
             $node_name .= (Config::get('disable_sub_mu_port') ? '' : ' - ' . $mu_port . ' 单端口');
         }
