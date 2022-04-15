4aecbca005eac76d4fa2773cc7dcd372a50f3e9a
olperson <41310813+olperson@users.noreply.github.com>
2022-04-15 18:34:28 +0800

    Update UserController.php
    -

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -113,22 +113,22 @@ class UserController extends BaseController
 		$bought=Bought::whereIn('shopid', $shopid)
 		->where('userid',$this->user->id)->orderby('id','desc')->first();
 		if (time()==0){
-		$shop = $bought->shop();
-		if (strtotime($this->user->expire_in)>time() && $this->user->class>0){
-		$nowtime=(int)time()/86400;
-                $bought_time=(int)$bought->datetime/86400
+		//$shop = $bought->shop();
+		//if (strtotime($this->user->expire_in)>time() && $this->user->class>0){
+		//$nowtime=(int)time()/86400;
+               // $bought_time=(int)$bought->datetime/86400
                 
-		while(($nowtime - $bought_time) % $shop->reset() == 0){
-		$nowtime=$nowtime+1;
-		}
-                $reset_time=$nowtime;
+		//while(($nowtime - $bought_time) % $shop->reset() == 0){
+		//$nowtime=$nowtime+1;
+		//}
+              //  $reset_time=$nowtime;
 		//$reset_time=(($nowtime-$bought->datetime)/86400) % $shop->reset();
-               if ($reset_time<=0){
-                $reset_time=$shop->reset();
-                }
-		}else{
-			$reset_time='Na';
-		}
+              // if ($reset_time<=0){
+              //  $reset_time=$shop->reset();
+             //   }
+		//}else{
+		//	$reset_time='Na';
+	//	}
 		}else{
 			$reset_time='Na';
 		}
